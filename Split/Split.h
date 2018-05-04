bee216b5e8a9078e65abb3befe91cc0a9d5c9260
David Boothe <dboothe@ancestry.com>
2018-05-04 08:39:43 -0600

    Add Carthage support

---
+++ Split/Split.h
@@ -0,0 +1,19 @@
+//
+//  Split.h
+//  Split
+//
+//  Created by David Boothe on 5/4/18.
+//  Copyright © 2018 David Boothe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Split.
+FOUNDATION_EXPORT double SplitVersionNumber;
+
+//! Project version string for Split.
+FOUNDATION_EXPORT const unsigned char SplitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Split/PublicHeader.h>
+
+
